e88a3523ebf1a07024ebeaf391481fab7d0f7246
Alexander Fisher <afisher@alexander-fisher.com>
2020-06-13 18:10:19 -0400

    Implement rest of API.

--- spring-boot/src/main/java/com/alexanderfisher/portfolio/api/springboot/service/impl/ProjectsApiDelegateImpl.java
+++ spring-boot/src/main/java/com/alexanderfisher/portfolio/api/springboot/service/impl/ProjectsApiDelegateImpl.java
@@ -1,11 +1,9 @@
 package com.alexanderfisher.portfolio.api.springboot.service.impl;
 
 import com.alexanderfisher.portfolio.api.hibernate.entities.ProjectsEntity;
-import com.alexanderfisher.portfolio.api.hibernate.entities.ProjectsEntity_;
-import com.alexanderfisher.portfolio.api.springboot.api.CategoriesApiDelegate;
-import com.alexanderfisher.portfolio.api.hibernate.entities.CategoriesEntity;
-import com.alexanderfisher.portfolio.api.models.Category;
 import com.alexanderfisher.portfolio.api.models.Project;
+import com.alexanderfisher.portfolio.api.models.ProjectGallery;
+import com.alexanderfisher.portfolio.api.springboot.api.ProjectsApiDelegate;
 import com.alexanderfisher.portfolio.api.springboot.service.EntityToModelConverter;
 import com.alexanderfisher.portfolio.api.springboot.service.SessionFactoryProvider;
 import org.hibernate.Session;
@@ -16,53 +14,47 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class CategoriesApiDelegateImpl implements CategoriesApiDelegate {
+public class ProjectsApiDelegateImpl implements ProjectsApiDelegate {
 
     private final SessionFactoryProvider sessionFactoryProvider;
 
-    public CategoriesApiDelegateImpl(SessionFactoryProvider sessionFactoryProvider) {
+    public ProjectsApiDelegateImpl(SessionFactoryProvider sessionFactoryProvider) {
         this.sessionFactoryProvider = sessionFactoryProvider;
     }
 
     @Override
-    public ResponseEntity<List<Project>> categoriesCategoryIdProjectsGet(Integer categoryId) {
+    public ResponseEntity<List<Project>> projectsGet() {
         Session session = sessionFactoryProvider.getSessionFactory().openSession();
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
         CriteriaQuery<ProjectsEntity> criteriaQuery = criteriaBuilder.createQuery(ProjectsEntity.class);
-        Root<ProjectsEntity> root = criteriaQuery.from(ProjectsEntity.class);
-        criteriaQuery.select(root).where(criteriaBuilder.equal(root.get(ProjectsEntity_.categoriesEntity), categoryId));
+        criteriaQuery.from(ProjectsEntity.class);
         Transaction transaction = session.beginTransaction();
         List<ProjectsEntity> projectsEntities = session.createQuery(criteriaQuery).getResultList();
         transaction.commit();
         session.close();
 
         List<Project> categories = projectsEntities.stream()
-            .map(EntityToModelConverter::toProject)
-            .collect(Collectors.toList());
+                .map(EntityToModelConverter::toProject)
+                .collect(Collectors.toList());
 
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<List<Category>> categoriesGet() {
+    public ResponseEntity<ProjectGallery> projectsProjectIdGalleryGet(Integer projectId) {
         Session session = sessionFactoryProvider.getSessionFactory().openSession();
-        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaQuery<CategoriesEntity> criteriaQuery = criteriaBuilder.createQuery(CategoriesEntity.class);
-        criteriaQuery.from(CategoriesEntity.class);
         Transaction transaction = session.beginTransaction();
-        List<CategoriesEntity> categoriesEntities = session.createQuery(criteriaQuery).getResultList();
+        ProjectsEntity projectsEntity = session.get(ProjectsEntity.class, projectId);
+
+        ProjectGallery projectGallery = EntityToModelConverter.toProjectGallery(projectsEntity);
+
         transaction.commit();
         session.close();
 
-        List<Category> categories = categoriesEntities.stream()
-            .map(EntityToModelConverter::toCategory)
-            .collect(Collectors.toList());
-
-        return new ResponseEntity<>(categories, HttpStatus.OK);
+        return new ResponseEntity<>(projectGallery, HttpStatus.OK);
     }
 }
